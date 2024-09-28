002855bb6d00e7621d8e1aa5834c89c1b072f770
wols <wolsey.rubio@mercadolibre.com.co>
2024-09-28 16:32:29 -0500

    add dialog and component

---
+++ ReelBuy/ReelBuy.Frontend/Shared/ConfirmDialog.razor.cs
@@ -0,0 +1,23 @@
+using ReelBuy.Shared.Resources;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using MudBlazor;
+
+namespace ReelBuy.Frontend.Shared;
+
+public partial class ConfirmDialog
+{
+    [CascadingParameter] private MudDialogInstance MudDialog { get; set; } = null!;
+    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+    [Parameter] public string Message { get; set; } = null!;
+
+    private void Accept()
+    {
+        MudDialog.Close(DialogResult.Ok(true));
+    }
+
+    private void Cancel()
+    {
+        MudDialog.Close(DialogResult.Cancel());
+    }
+}
