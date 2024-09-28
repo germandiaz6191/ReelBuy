bcad7fc2832f3c2fae6c826ae8b645ed0ba3a90e
codencode <rugil22@hotmail.com>
2024-09-28 17:40:52 -0500

    add filter and paginete in frontend

--- ReelBuy/ReelBuy.Frontend/Pages/Countries/CountryCreate.razor.cs
+++ ReelBuy/ReelBuy.Frontend/Pages/Countries/CountryCreate.razor.cs
@@ -1,9 +1,9 @@
-using CurrieTechnologies.Razor.SweetAlert2;
+using ReelBuy.Frontend.Repositories;
+using ReelBuy.Shared.Resources;
+using ReelBuy.Shared.Entities;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
-using ReelBuy.Frontend.Repositories;
-using ReelBuy.Shared.Entities;
-using ReelBuy.Shared.Resources;
+using MudBlazor;
 
 namespace ReelBuy.Frontend.Pages.Countries;
 
@@ -14,7 +14,7 @@ public partial class CountryCreate
 
     [Inject] private IRepository Repository { get; set; } = null!;
     [Inject] private NavigationManager NavigationManager { get; set; } = null!;
-    [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
     [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
 
     private async Task CreateAsync()
@@ -23,19 +23,12 @@ public partial class CountryCreate
         if (responseHttp.Error)
         {
             var message = await responseHttp.GetErrorMessageAsync();
-            await SweetAlertService.FireAsync(Localizer["Error"], Localizer[message!], SweetAlertIcon.Error);
+            Snackbar.Add(Localizer[message], Severity.Error);
             return;
         }
 
         Return();
-        var toast = SweetAlertService.Mixin(new SweetAlertOptions
-        {
-            Toast = true,
-            Position = SweetAlertPosition.BottomEnd,
-            ShowConfirmButton = true,
-            Timer = 3000
-        });
-        await toast.FireAsync(icon: SweetAlertIcon.Success, message: Localizer["RecordCreatedOk"]);
+        Snackbar.Add(Localizer["RecordCreatedOk"], Severity.Success);
     }
 
     private void Return()
