ad6741cecfd27f3cfc4e6185f3044f09a8b79606
Luisa Arcila Cuervo <luisa.arcila@hotmail.com>
2024-09-28 18:41:22 -0500

    Add pagenate and filter Categories

--- ReelBuy/ReelBuy.Frontend/Pages/Categories/CategoriesIndex.razor.cs
+++ ReelBuy/ReelBuy.Frontend/Pages/Categories/CategoriesIndex.razor.cs
@@ -1,78 +1,150 @@
-using CurrieTechnologies.Razor.SweetAlert2;
-using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.Localization;
+using System.Net;
 using ReelBuy.Frontend.Repositories;
 using ReelBuy.Shared.Resources;
+using ReelBuy.Frontend.Shared;
+using ReelBuy.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using MudBlazor;
 
 namespace ReelBuy.Frontend.Pages.Categories;
 
 public partial class CategoriesIndex
 {
-    [Inject] private IRepository Repository { get; set; } = null!;
+    private List<Category>? Categories { get; set; }
+    private MudTable<Category> table = new();
+    private readonly int[] pageSizeOptions = { 10, 25, 50, int.MaxValue };
+    private int totalRecords = 0;
+    private bool loading;
+    private const string baseUrl = "api/categories";
+    private string infoFormat = "{first_item}-{last_item} => {all_items}";
+
     [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private IDialogService DialogService { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
     [Inject] private NavigationManager NavigationManager { get; set; } = null!;
-    [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
-    public List<ReelBuy.Shared.Entities.Category>? Categories { get; set; }
+
+    [Parameter, SupplyParameterFromQuery] public string Filter { get; set; } = string.Empty;
 
     protected override async Task OnInitializedAsync()
     {
-        await LoadAsync();
+        await LoadTotalRecordsAsync();
     }
 
-    private async Task LoadAsync()
+    private async Task LoadTotalRecordsAsync()
     {
-        var responseHppt = await Repository.GetAsync<List<ReelBuy.Shared.Entities.Category>>("api/categories");
-        if (responseHppt.Error)
-        {
-            var message = await responseHppt.GetErrorMessageAsync();
-            await SweetAlertService.FireAsync(Localizer["Error"], message, SweetAlertIcon.Error);
-            return;
-        }
-        Categories = responseHppt.Response!;
-    }
+        loading = true;
+        var url = $"{baseUrl}/totalRecordsPaginated";
 
-    private async Task DeleteAsync(ReelBuy.Shared.Entities.Category category)
-    {
-        var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+        if (!string.IsNullOrWhiteSpace(Filter))
         {
-            Title = Localizer["Confirmation"],
-            Text = string.Format(Localizer["DeleteConfirm"], Localizer["Categories"], category.Name),
-            Icon = SweetAlertIcon.Question,
-            ShowCancelButton = true,
-            CancelButtonText = Localizer["Cancel"]
-        });
-
-        var confirm = string.IsNullOrEmpty(result.Value);
-
-        if (confirm)
-        {
-            return;
+            url += $"?filter={Filter}";
         }
 
-        var responseHttp = await Repository.DeleteAsync($"api/categories/{category.Id}");
+        var responseHttp = await Repository.GetAsync<int>(url);
         if (responseHttp.Error)
         {
-            if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(Localizer[message], Severity.Error);
+            return;
+        }
+
+        totalRecords = responseHttp.Response;
+        loading = false;
+    }
+
+    private async Task<TableData<Category>> LoadListAsync(TableState state, CancellationToken cancellationToken)
+    {
+        int page = state.Page + 1;
+        int pageSize = state.PageSize;
+        var url = $"{baseUrl}/paginated/?page={page}&recordsnumber={pageSize}";
+
+        if (!string.IsNullOrWhiteSpace(Filter))
+        {
+            url += $"&filter={Filter}";
+        }
+
+        var responseHttp = await Repository.GetAsync<List<Category>>(url);
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(Localizer[message], Severity.Error);
+            return new TableData<Category> { Items = [], TotalItems = 0 };
+        }
+        if (responseHttp.Response == null)
+        {
+            return new TableData<Category> { Items = [], TotalItems = 0 };
+        }
+        return new TableData<Category>
+        {
+            Items = responseHttp.Response,
+            TotalItems = totalRecords
+        };
+    }
+
+    private async Task SetFilterValue(string value)
+    {
+        Filter = value;
+        await LoadTotalRecordsAsync();
+        await table.ReloadServerData();
+    }
+
+    private async Task ShowModalAsync(int id = 0, bool isEdit = false)
+    {
+        var options = new DialogOptions() { CloseOnEscapeKey = true, CloseButton = true };
+        IDialogReference? dialog;
+        if (isEdit)
+        {
+            var parameters = new DialogParameters
+                {
+                    { "Id", id }
+                };
+            dialog = DialogService.Show<CategoryEdit>($"{Localizer["Edit"]} {Localizer["Category"]}", parameters, options);
+        }
+        else
+        {
+            dialog = DialogService.Show<CategoryCreate>($"{Localizer["New"]} {Localizer["Category"]}", options);
+        }
+
+        var result = await dialog.Result;
+        if (result!.Canceled)
+        {
+            await LoadTotalRecordsAsync();
+            await table.ReloadServerData();
+        }
+    }
+
+    private async Task DeleteAsync(Category category)
+    {
+        var parameters = new DialogParameters
             {
-                NavigationManager.NavigateTo("/");
+                { "Message", string.Format(Localizer["DeleteConfirm"], Localizer["Category"], category.Name) }
+            };
+        var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall, CloseOnEscapeKey = true };
+        var dialog = DialogService.Show<ConfirmDialog>(Localizer["Confirmation"], parameters, options);
+        var result = await dialog.Result;
+        if (result!.Canceled)
+        {
+            return;
+        }
+
+        var responseHttp = await Repository.DeleteAsync($"{baseUrl}/{category.Id}");
+        if (responseHttp.Error)
+        {
+            if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+            {
+                NavigationManager.NavigateTo("/categories");
             }
             else
             {
-                var mensajeError = await responseHttp.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync(Localizer["Error"], mensajeError, SweetAlertIcon.Error);
+                var message = await responseHttp.GetErrorMessageAsync();
+                Snackbar.Add(Localizer[message], Severity.Error);
             }
             return;
         }
-
-        await LoadAsync();
-        var toast = SweetAlertService.Mixin(new SweetAlertOptions
-        {
-            Toast = true,
-            Position = SweetAlertPosition.BottomEnd,
-            ShowConfirmButton = true,
-            Timer = 3000,
-            ConfirmButtonText = Localizer["Yes"]
-        });
-        await toast.FireAsync(icon: SweetAlertIcon.Success, message: Localizer["RecordDeletedOk"]);
+        await LoadTotalRecordsAsync();
+        await table.ReloadServerData();
+        Snackbar.Add(Localizer["RecordDeletedOk"], Severity.Success);
     }
 }
--- ReelBuy/ReelBuy.Frontend/Pages/Categories/CategoryEdit.razor.cs
+++ ReelBuy/ReelBuy.Frontend/Pages/Categories/CategoryEdit.razor.cs
@@ -1,7 +1,6 @@
-using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
-using ReelBuy.Frontend.Pages.Categories;
+using MudBlazor;
 using ReelBuy.Frontend.Repositories;
 using ReelBuy.Shared.Entities;
 using ReelBuy.Shared.Resources;
@@ -15,14 +14,14 @@ public partial class CategoryEdit
 
     [Inject] private NavigationManager NavigationManager { get; set; } = null!;
     [Inject] private IRepository Repository { get; set; } = null!;
-    [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
     [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
 
     [Parameter] public int Id { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
-        var responseHttp = await Repository.GetAsync<ReelBuy.Shared.Entities.Category>($"api/categories/{Id}");
+        var responseHttp = await Repository.GetAsync<Category>($"api/categories/{Id}");
 
         if (responseHttp.Error)
         {
@@ -33,7 +32,7 @@ public partial class CategoryEdit
             else
             {
                 var messageError = await responseHttp.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync(Localizer["Error"], messageError, SweetAlertIcon.Error);
+                Snackbar.Add(messageError!, Severity.Error);
             }
         }
         else
@@ -48,20 +47,13 @@ public partial class CategoryEdit
 
         if (responseHttp.Error)
         {
-            var mensajeError = await responseHttp.GetErrorMessageAsync();
-            await SweetAlertService.FireAsync(Localizer["Error"], mensajeError, SweetAlertIcon.Error);
+            var messageError = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(messageError!, Severity.Error);
             return;
         }
 
         Return();
-        var toast = SweetAlertService.Mixin(new SweetAlertOptions
-        {
-            Toast = true,
-            Position = SweetAlertPosition.BottomEnd,
-            ShowConfirmButton = true,
-            Timer = 3000
-        });
-        await toast.FireAsync(icon: SweetAlertIcon.Success, message: Localizer["RecordSavedOk"]);
+        Snackbar.Add(Localizer["RecordSavedOk"], Severity.Success);
     }
 
     private void Return()
