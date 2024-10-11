4a70fef8988c92012dccc86db5d69411a3f82fd1
German Díaz <germandiaz6191@gmail.com>
2024-10-11 14:52:10 -0500

    add Change Pass and editUser

---
+++ ReelBuy/ReelBuy.Frontend/Pages/Auth/ChangePassword.razor.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using MudBlazor;
+using ReelBuy.Frontend.Repositories;
+using ReelBuy.Shared.DTOs;
+using ReelBuy.Shared.Resources;
+
+namespace ReelBuy.Frontend.Pages.Auth;
+
+public partial class ChangePassword
+{
+    private ChangePasswordDTO changePasswordDTO = new();
+    private bool loading;
+
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private IDialogService DialogService { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+    [CascadingParameter] private MudDialogInstance MudDialog { get; set; } = null!;
+
+    private async Task ChangePasswordAsync()
+    {
+        loading = true;
+        var responseHttp = await Repository.PostAsync("/api/accounts/changePassword", changePasswordDTO);
+        loading = false;
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(Localizer[message], Severity.Error);
+            return;
+        }
+
+        MudDialog.Cancel();
+        NavigationManager.NavigateTo("/EditUser");
+        Snackbar.Add(Localizer["PasswordChangedSuccessfully"], Severity.Success);
+    }
+
+    private void ReturnAction()
+    {
+        MudDialog.Cancel();
+        NavigationManager.NavigateTo("/EditUser");
+    }
+}
