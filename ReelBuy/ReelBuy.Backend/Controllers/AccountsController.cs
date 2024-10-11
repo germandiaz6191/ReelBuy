8c27005b98a970f977169dd88df4ff9395072c9f
German Díaz <germandiaz6191@gmail.com>
2024-10-10 21:52:48 -0500

    add functionallity user registry

--- ReelBuy/ReelBuy.Backend/Controllers/AccountsController.cs
+++ ReelBuy/ReelBuy.Backend/Controllers/AccountsController.cs
@@ -125,7 +125,7 @@ public class AccountsController : ControllerBase
         };
     }
 
-    public async Task<ActionResponse<string>> SendConfirmationEmailAsync(User user, string language)
+    private async Task<ActionResponse<string>> SendConfirmationEmailAsync(User user, string language)
     {
         var myToken = await _usersUnitOfWork.GenerateEmailConfirmationTokenAsync(user);
         var tokenLink = Url.Action("ConfirmEmail", "accounts", new
