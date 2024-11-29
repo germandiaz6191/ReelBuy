e0881c7c23a97eb0348d9375d8bc4f6a06f3693c
German Díaz <germandiaz6191@gmail.com>
2024-11-28 20:06:36 -0500

    change size

--- ReelBuy/ReelBuy.Frontend/Layout/MainLayout.razor.cs
+++ ReelBuy/ReelBuy.Frontend/Layout/MainLayout.razor.cs
@@ -13,6 +13,36 @@ public partial class MainLayout
     private bool _darkMode { get; set; } = true;
     [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
 
+    private MudTheme CustomTheme => new MudTheme
+    {
+        PaletteLight = new PaletteLight
+        {
+            Primary = "rgb(74, 81, 94)",
+            Secondary = "rgb(63, 81, 181)",
+            Background = "rgb(245, 245, 245)",
+            Surface = "rgb(255, 255, 255)",
+            AppbarBackground = "rgb(74, 81, 94)",
+            AppbarText = "rgb(255, 255, 255)",
+            TextPrimary = "rgb(33, 33, 33)",
+            TextSecondary = "rgb(169, 169, 169)",
+            DrawerBackground = "rgb(238, 238, 238)",
+            DrawerText = "rgb(33, 33, 33)"
+        },
+        PaletteDark = new PaletteDark
+        {
+            Primary = "rgb(74, 81, 94)",
+            Secondary = "rgb(255, 159, 64)",
+            Background = "rgb(18, 18, 18)",
+            Surface = "rgb(51, 51, 51)",
+            AppbarBackground = "rgb(74, 81, 94)",
+            AppbarText = "rgb(255, 255, 255)",
+            TextPrimary = "rgb(255, 255, 255)",
+            TextSecondary = "rgb(169, 169, 169)",
+            DrawerBackground = "rgb(30, 30, 30)",
+            DrawerText = "rgb(255, 255, 255)"
+        }
+    };
+
     private void DrawerToggle()
     {
         _drawerOpen = !_drawerOpen;
