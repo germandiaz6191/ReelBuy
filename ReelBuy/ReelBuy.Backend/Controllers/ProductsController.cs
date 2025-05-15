2ec09b964f78e65cd6619d805d2acc78f54ad607
German Díaz <germandiaz6191@gmail.com>
2025-05-15 15:53:01 -0500

    delete comment

--- ReelBuy/ReelBuy.Backend/Controllers/ProductsController.cs
+++ ReelBuy/ReelBuy.Backend/Controllers/ProductsController.cs
@@ -345,8 +345,8 @@ public class ProductsController : GenericController<Product>
                 if (!string.IsNullOrEmpty(reel.ReelUri) && !reel.ReelUri.Contains(".mp4"))
                 {
                     var productReel = Convert.FromBase64String(reel.ReelUri);
-                    //reel.ReelUri = await _fileStorage.SaveFileAsync(productReel, ".mp4", "reels");
-                    reel.ReelUri = "https://azurerb2025.blob.core.windows.net/reels/15613a15-bb46-417c-90ad-d456853a2874.mp4";
+                    reel.ReelUri = await _fileStorage.SaveFileAsync(productReel, ".mp4", "reels");
+                    //reel.ReelUri = "https://azurerb2025.blob.core.windows.net/reels/15613a15-bb46-417c-90ad-d456853a2874.mp4";
                 }
             }
             model.Reels = CurrentProduct.Reels;
